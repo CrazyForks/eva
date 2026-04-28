331d45d6a3b2d2ca530649af20540cdb4e8342ce
hzq <usepr@hotmail.com>
2026-04-29 00:39:48 +0800

    eva成为微信bot

---
+++ showcase/wechat-bot/README.md
@@ -0,0 +1,32 @@
+# 微信-EVA
+
+## 快速开始
+
+1. 确认`eva`命令可使用（`python3 eva.py`首次运行过，并执行了`source ~/.bashrc`就会设置`eva`快捷脚本）
+2. 执行`python3 bot.py`
+3. 会输出一个链接，请用浏览器打开并通过微信扫码登录
+
+
+## 关于wechatbot
+
+微信官方通过 iLink 协议来对接ClawBot，也对接对接其他自定义机器人。
+
+wechatbot则是一个已封装好 iLink 协议的python库，说明文档：https://www.wechatbot.dev/en/python
+
+
+## 给EVA配的hints.md
+
+我的各种信息以及笔记都记在Obsidian里，我希望EVA能帮我检索这些内容，尤其各种杂乱的账号密码。
+此外，Tavily可以让EVA具备联网检索功能。我给EVA配的`hints.md`内容如下（`hints.md`参照EVA项目说明文档）：
+
+```md
+## 我的笔记本
+
+存储目录：/root/Obsidian，里面记录了各种账号信息等
+
+## 我的Tavily API key
+
+tvly-dev-xxxxx-xxxxxxxxxxxxxxxxxxxx
+
+可用于联网搜索
+```
---
+++ showcase/wechat-bot/bot.py
@@ -0,0 +1,48 @@
+from wechatbot import WeChatBot
+import os
+import subprocess
+
+def run_cli(args, timeout: int = 300):
+    try:
+        result = subprocess.run(
+            args,
+            capture_output=True,
+            text=True,
+            errors='replace',
+            cwd=os.getcwd(),
+            timeout=timeout,
+            shell=False
+        )
+        output = f"{result.stdout}"
+        if result.stderr:
+            output += f"\nSTDERR:\n{result.stderr}"
+        return output.strip() or "(no output)"
+    except Exception as e:
+        return f"执行失败：{str(e)}"
+
+
+def clean_eva_output(out: str):
+    eva_s = "[*] EVA:"
+    if eva_s in out:
+        out = out[out.rindex(eva_s)+len(eva_s):]
+    
+    session_s = "会话已保存到："
+    if session_s in out:
+        out = out[:out.rindex(session_s)]
+
+    return out.strip().replace('\033[2m', '').replace('\033[0m', '\n--\n')
+
+
+bot = WeChatBot()
+
+@bot.on_message
+async def handle(msg):
+    await bot.send_typing(msg.user_id)
+    text = msg.text.strip().lower()
+    if text in ['/clear', 'clear']:
+        args = ["eva", "-c"]
+    else:
+        args = ["eva", "-asu", text]
+    await bot.reply(msg, clean_eva_output(run_cli(args)))
+
+bot.run()
